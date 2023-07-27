0c16e5cca91aaec73e3bfc554c8093b594f246f1
Pablo Cortez <cspablocortez@gmail.com>
2023-07-26 17:47:36 -0700

    Open file works!

--- webapp/static/js/quill-editor.js
+++ webapp/static/js/quill-editor.js
@@ -21,6 +21,37 @@ function downloadTextAsFile() {
     downloadLink.click();
 }
 
+
+function readFileContents(file) {
+  const reader = new FileReader();
+
+  reader.onload = function(event) {
+    const contents = event.target.result;
+
+    // Populate Title
+    documentTitle.innerText = file.name;
+
+    // Populate Body
+    quill.setText(contents);
+  };
+
+  reader.onerror = function(event) {
+    console.error("Error reading the file:", event.target.error);
+  };
+
+  // Read the file as text
+  reader.readAsText(file);
+}
+
+// Event listener to trigger file reading when a file is selected
+document.getElementById("fileInput").addEventListener("change", function(event) {
+  const file = event.target.files[0];
+  if (file) {
+    readFileContents(file);
+  }
+});
+
+
 quill.on('text-change', function(delta, oldDelta, source) {
   if (source == 'user') {
     let text = quill.getText();
