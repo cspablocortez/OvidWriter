f17b0818bd943fb5b72ace463182d7e07de854c6
Pablo Cortez <cspablocortez@gmail.com>
2024-03-16 21:04:41 -0400

    Service worker

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,42 @@
-self.addEventListener('install', function(e) {
-    e.waitUntil(
-      caches.open('my-app').then(function(cache) {
-        return cache.addAll([
-          '/',
-          '/index.html',
-          'css/normalize.css',
-          'css/skeleton.css',
-          'css/settings.css',
-          'css/style.css',
-          'js/main.js',
-          'js/quill-config.js',
-          'js/storage.js',
-          'js/settings.js',
-          'js/UI.js',
-          '/icons/*'
-        ]);
-      })
-    );
-   });
-   
-   self.addEventListener('fetch', function(event) {
-    console.log(event.request.url);
-    event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      })
-    );
-   });
-   
+const VERSION = "v1"
+const CACHE_NAME = `ovid-writer=${VERSION}`
+const APP_STATIC_RESOURCES = [
+  '/',
+  '/index.html',
+  'css/normalize.css',
+  'css/skeleton.css',
+  'css/settings.css',
+  'css/style.css',
+  'js/main.js',
+  'js/quill-config.js',
+  'js/storage.js',
+  'js/settings.js',
+  'js/UI.js',
+  'icons/*'
+]
+
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    (async () => {
+      const cache = await caches.open(CACHE_NAME);
+      cache.addAll(APP_STATIC_RESOURCES);
+    })(),
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    (async () => {
+      const names = await caches.keys();
+      await Promise.all(
+        names.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        }),
+      );
+      await clients.claim();
+    })(),
+  );
+});
