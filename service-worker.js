19c9f68e4ef0088f2924ba58dd06d24fffefa932
Pablo Cortez <cspablocortez@gmail.com>
2024-03-20 21:16:05 -0400

    Update service-worker.js
    Momentary disabling of the cache.

--- service-worker.js
+++ service-worker.js
@@ -34,41 +34,41 @@ self.addEventListener("install", (event) => {
   );
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      const names = await caches.keys();
-      await Promise.all(
-        names.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        }),
-      );
-      await clients.claim();
-    })(),
-  );
-});
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(
+//     (async () => {
+//       const names = await caches.keys();
+//       await Promise.all(
+//         names.map((name) => {
+//           if (name !== CACHE_NAME) {
+//             return caches.delete(name);
+//           }
+//         }),
+//       );
+//       await clients.claim();
+//     })(),
+//   );
+// });
 
-self.addEventListener("fetch", (event) => {
-  // when seeking an HTML page
-  if (event.request.mode === "navigate") {
-    // Return to the index.html page
-    event.respondWith(caches.match("index.html"));
-    return;
-  }
+// self.addEventListener("fetch", (event) => {
+//   // when seeking an HTML page
+//   if (event.request.mode === "navigate") {
+//     // Return to the index.html page
+//     event.respondWith(caches.match("index.html"));
+//     return;
+//   }
 
-  // For every other request type
-  event.respondWith(
-    (async () => {
-      const cache = await caches.open(CACHE_NAME);
-      const cachedResponse = await cache.match(event.request.url);
-      if (cachedResponse) {
-        // Return the cached response if it's available.
-        return cachedResponse;
-      }
-      // Respond with a HTTP 404 response status.
-      return new Response(null, { status: 404 });
-    })(),
-  );
-});
+//   // For every other request type
+//   event.respondWith(
+//     (async () => {
+//       const cache = await caches.open(CACHE_NAME);
+//       const cachedResponse = await cache.match(event.request.url);
+//       if (cachedResponse) {
+//         // Return the cached response if it's available.
+//         return cachedResponse;
+//       }
+//       // Respond with a HTTP 404 response status.
+//       return new Response(null, { status: 404 });
+//     })(),
+//   );
+// });
